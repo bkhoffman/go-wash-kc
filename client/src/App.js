62e95d9fca8410b75bbb2d6f82d49b99246f15e8
JD <jdhouse27@gmail.com>
2019-07-11 20:29:11 -0500

    added react routers and component initial builds

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import Login from "./components/Login";
+import About from "./components/pages/About";
+import Packages from "./components/pages/Packages";
+import Cart from "./components/pages/Cart";
+import Payment from "./components/pages/Payment";
+import Calendar from "./components/pages/Calendar";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+        <div>
+          <NavTabs />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/packages" component={Packages} />
+          <Route exact path="/mycart" component={Cart} />
+          <Route exact path="/paynow" component={Payment} />
+          <Route exact path="/calendar" component={Calendar} />
+        </div>
+      </Router>
+    );
 }
 
 export default App;
