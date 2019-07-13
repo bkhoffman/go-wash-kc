7bcc194fd21f9a03222556538d331b99a160f802
Brad <bkhoffman@gmail.com>
2019-07-13 11:14:12 -0500

    cart working, new images folder

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
- const Navbar = () => {
+ const Navbar = ()=>{
     return(
             <nav className="nav-wrapper">
                 <div className="container">
-                    <Link to="/" className="brand-logo">Shopping</Link>
+                    <Link to="/" className="brand-logo">Go Wash KC</Link>
                     
                     <ul className="right">
+                        <li><Link to="/">Packages</Link></li>
                         <li><Link to="/cart">My cart</Link></li>
                         <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     </ul>
                 </div>
-            </nav>  
+            </nav>
+   
+        
     )
 }
 
