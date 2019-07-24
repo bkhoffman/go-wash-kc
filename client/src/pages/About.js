7b3b3fa1abbeede808daa39a3643fa5679b39d57
Wired-Dollar <wired.1.dollar@gmail.com>
2019-07-23 19:44:39 -0500

    added text for About us page

---
+++ client/src/pages/About.js
@@ -0,0 +1,24 @@
+Here at Go Wash KCTM we thrive off shinny things, shinny paint, shinny chrome, shinny consoles, shinny dash, shinny buttons and dials, shinny smiles. We go the extra miles. 
+
+Go Wash KCTM offers a money back garauntee on all of our services1. If you are unsatified with any of our work and we are not able to fix it to your liking then we will give you your money back2.
+
+Go Wash KCTM saves you time and money.3 We offer peace of mind and the stress relief of not needing to DIY everything. Relax in the confidence of knowing that your vehicle is being treated with only the highest quality materials by experienced professionals. 
+
+Go Wash KCTM lets you have one less thing to have to manage time for in your busy schedule.4 You can go about your normal routine and we will come to you and detail your car while you attend to more important things in life. Save the time you would be spending sitting at a business having your car cleaned and have more time at home, the office, or your favorite recreational activity.3
+
+Go Wash KCTM can make your car shinny inside and out, garaunteed to make your smile shine.
+
+Thank you for your business! Have a fabulous day! 
+
+Your friends,
+
+
+Go Wash KCTM 
+:)
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+1 - Money back garauntee is subject to terms and conditions and may not include a full refund or compensation for all services.
+2 - Exceptions to refund are made for permanent cosmetic damage (burns, cuts, tears, breaks, deep stains, etc)
+3 - Savings compared to main competitors in local area or traveling to detailing station/ car wash and paying for services and materials.
+4 - Go Wash KCTM is not a schedule assistance or time management program. We offer services that are able to be adaptable with location and time for your convienience.
