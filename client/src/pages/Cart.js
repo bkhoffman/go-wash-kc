d8c83e20f24275a5a50ccf101af279ccce066392
Brad <bkhoffman@gmail.com>
2019-07-24 23:52:28 -0500

    datepicker mostly functional, form added to cart page and checkout button moved

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import API from "../utils/API";
 import { removeItem,addQuantity,subtractQuantity} from '../Components/actions/cartActions'
 // import Calendar from './Calendar'
+
+import { Input, FormBtn } from "../Components/Form";
+
 import DatePicker from 'react-datepicker';
+// import setMinutes from 'date-fns/setMinutes'
+// import setHours from 'date-fns/setHours'
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -26,6 +32,53 @@ class Cart extends Component{
           startDate: date
         });
     }
+    //form info
+    state = {
+        // User: [],
+        userName: "",
+        password: "",
+        firstName: "",
+        lastName: "",
+        address: "",
+        phone: "",
+        email: ""
+      };
+
+      componentDidMount() {
+        this.loadUser();
+      }
+      
+      loadUser = () => {
+        API.getUser()
+          .then(res => this.setState({ User: res.data}))
+          .catch(err => console.log(err));
+      };
+      
+      handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+    //Checkout button
+    handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("whats up hommies")
+        if (this.state.userName && this.state.password) {
+          API.saveUser({
+            userName: this.state.userName,
+            password: this.state.password,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            address: this.state.address,
+            phone: this.state.phone,
+            email: this.state.email
+
+          })
+            .then(res => this.loadUser())
+            .catch(err => console.log(err));
+        }
+      };
    
     render(){
               
@@ -65,22 +118,69 @@ class Cart extends Component{
                 <div>
                     <span className="collection-item"><b>Total: ${this.props.total} </b></span>
                     <br></br>
-                    <button className="waves-effect waves-light btn">Checkout</button>
+                    
                 </div>
             </div>
+            <form className="appointmentForm">
             <div>
                 <DatePicker 
                     placeholderText="Click to schedule"
                     selected={this.state.startDate}
-                    onSelect={this.handleSelect}
+                    // onSelect={this.handleSelect}
                     onChange={this.handleChange}
                     showTimeSelect
-                    timeFormat="HH:mm"
+                    // minTime={setHours(setMinutes(new Date(), 0),8)}
+                    // maxTime={setHours(setMinutes(new Date(), 0), 16)}
+                    timeFormat="p"
                     timeIntervals={60}
                     dateFormat="MMMM d, yyyy h:mm aa"
-                    timeCaption="time"
+                    timeCaption="Time"
                 />
             </div>
+                <Input
+                    value={this.state.firstName}
+                    // onChange={this.handleInputChange}
+                    name="firstName"
+                    placeholder="First Name (required)"
+                />
+                <Input
+                    value={this.state.lastName}
+                    // onChange={this.handleInputChange}
+                    name="lastName"
+                    placeholder="Last Name (required)"
+                />
+                <Input
+                    value={this.state.carMake}
+                    // onChange={this.handleInputChange}
+                    name="carMake"
+                    placeholder="Car Make (required)"
+                />
+                <Input
+                    value={this.state.carModel}
+                    // onChange={this.handleInputChange}
+                    name="carModel"
+                    placeholder="Car Model (required)"
+                />
+                <Input
+                    value={this.state.userLicencePlate}
+                    // onChange={this.handleInputChange}
+                    name="licence"
+                    placeholder="Licence Plate Number (required)"
+                />
+                <Input
+                    value={this.state.userLocation}
+                    onChange={this.handleInputChange}
+                    name="location"
+                    placeholder="Location of Service (required)"
+                /> 
+                <button className="waves-effect waves-light btn checkout">Checkout</button>
+                {/* <FormBtn
+                    disabled={!(this.state.userName && this.state.password)}
+                    onClick={this.handleFormSubmit}
+                >
+                    Create Account
+                </FormBtn> */}
+            </form>
             {/* <Calendar /> */}
         </div>
        )
