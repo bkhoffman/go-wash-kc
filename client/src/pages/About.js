569e027f86ed6eec6b1f03ee27e4d90348cf86dc
Wired-Dollar <wired.1.dollar@gmail.com>
2019-07-27 10:23:29 -0500

    formatting

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,22 +5,22 @@ function About() {
    <div>
      <h1>Hey guys! I'm a header!</h1>
      <h2>Things I like!</h2>
-     <h4>Here at Go Wash KCTM we thrive off shinny things, shinny paint, shinny chrome, shinny consoles, shinny dash, shinny buttons and dials, shinny smiles. We go the extra miles. 
+     <h4>Here at Go Wash KC we thrive off shinny things, shinny paint, shinny chrome, shinny consoles, shinny dash, shinny buttons and dials, shinny smiles. We go the extra miles. 
 
-     Go Wash KCTM offers a money back garauntee on all of our services1. If you are unsatified with any of our work and we are not able to fix it to your liking then we will give you your money back2.
+     Go Wash KC offers a money back garauntee on all of our services1. If you are unsatified with any of our work and we are not able to fix it to your liking then we will give you your money back2.
 
-     Go Wash KCTM saves you time and money.3 We offer peace of mind and the stress relief of not needing to DIY everything. Relax in the confidence of knowing that your vehicle is being treated with only the highest quality materials by experienced professionals. 
+     Go Wash KC saves you time and money.3 We offer peace of mind and the stress relief of not needing to DIY everything. Relax in the confidence of knowing that your vehicle is being treated with only the highest quality materials by experienced professionals. 
 
-     Go Wash KCTM lets you have one less thing to have to manage time for in your busy schedule.4 You can go about your normal routine and we will come to you and detail your car while you attend to more important things in life. Save the time you would be spending sitting at a business having your car cleaned and have more time at home, the office, or your favorite recreational activity.3
+     Go Wash KC lets you have one less thing to have to manage time for in your busy schedule.4 You can go about your normal routine and we will come to you and detail your car while you attend to more important things in life. Save the time you would be spending sitting at a business having your car cleaned and have more time at home, the office, or your favorite recreational activity.3
 
-     Go Wash KCTM can make your car shinny inside and out, garaunteed to make your smile shine.
+     Go Wash KC can make your car shinny inside and out, garaunteed to make your smile shine.
 
     Thank you for your business! Have a fabulous day! 
 
     Your friends,
 
 
-    Go Wash KCTM 
+    Go Wash KC 
     :)
 
 
