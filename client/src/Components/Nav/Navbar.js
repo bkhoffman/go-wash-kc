3bca51333e287406cdfd704729a21e41ccc19e26
Wired-Dollar <wired.1.dollar@gmail.com>
2019-07-30 19:06:02 -0500

    added logo to navbar

--- client/src/Components/Nav/Navbar.js
+++ client/src/Components/Nav/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import TemporaryDrawer from "Components/Drawer/Drawers"
+import TemporaryDrawer from "Components/Drawer/Drawers";
+import brandLogo from "../../images/Logo4.png";
+
 
 
  const Navbar = ()=>{
     return(
         <nav className="nav-wrapper">
             <div className="container">
-                <Link to="/" id="brand-logo">Go Wash KC</Link>
+                 
+                 {/* to="/" id="brandLogo" */}
                 
+                <ul className="left"><Link to= "/"><img src={brandLogo}></img></Link></ul>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/packages">Packages</Link></li>
@@ -16,6 +20,7 @@ import TemporaryDrawer from "Components/Drawer/Drawers"
                     <li><Link to="/cart"><i className="material-icons">shopping_cart</i></Link></li>
                     <li><TemporaryDrawer>Login</TemporaryDrawer></li>
                     <li><Link to="/signup">Signup</Link></li>
+                    {/* <Link><img src={brandLogo}></img></Link> */}
                 </ul>
             </div>
         </nav>
